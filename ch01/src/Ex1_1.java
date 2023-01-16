93519b93ae8cc656a18b69e96d22c6689a1fbf50
manjuSnack <dlekddl43@gmail.com>
2023-01-16 19:40:18 +0900

    Java의 정석, 기초편 - Chapter 01 ~ 12, 자바란? ~ Eclipse의 View, Perspective, Workspace
    Chapter 01 - 자바(Java)란?
    Chapter 02 - 자바의 역사
    Chapter 03 - 자바의 특징
    Chapter 04 - 자바 가상 머신 ( JVM, Java Virtual Machine )
    Chapter 05 - 자바 개발도구( JDK, Java Development Kit ) 설치하기
    Chapter 06 - 자바 개발도구( JDK, Java Development Kit ) 설정하기
    Chapter 07 - 자바 API ( Application Programing Interface )
    Chapter 08 - 자바 첫 프로그램 작성하기
    Chapter 09 - 자바 프로그램의 실행과정
    Chapter 10 - Eclipse 설치하기
    Chapter 11 - Eclipse로 Java 프로그램 개발하기
    Chapter 12 - Eclipse의 View, Perspective(관점), Workspace

---
+++ ch01/src/Ex1_1.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author manjuSnack
+ *
+ * 예제 1-1.
+ */
+public class Ex1_1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world."); // 화면에 글자를 출력한다.
+
+	}
+ 
+}   
