1b3bae4e99f202125e178fa5da992e533c67deac
manjuSnack <dlekddl43@gmail.com>
2023-04-01 21:33:02 +0900

    자바 3판 / ~p.21 복습

---
+++ ch02__Java_3/src/VarEx1.java
@@ -0,0 +1,19 @@
+import static java.lang.System.*;
+
+public class VarEx1 {
+    public static void main(String[] args) {
+        // int year = 0;
+        // int age = 14;
+
+        int year = 0, age = 14;
+
+        out.println(year);
+        out.println(age);
+
+        year = age + 2000;
+        age = age + 1;
+
+        out.println(year);
+        out.println(age);
+    }
+}
